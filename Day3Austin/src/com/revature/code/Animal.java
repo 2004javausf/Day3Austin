91c24d54a6b3546b12fdceaab74b69db8abec57f
Austin Coburn <austincoburn41@gmail.com>
2020-04-15 15:13:00 -0500

    Created an Animal object and driver

--- Day3Austin/src/com/revature/code/Animal.java
+++ Day3Austin/src/com/revature/code/Animal.java
@@ -1,5 +1,32 @@
 package com.revature.code;
 
 public class Animal {
-
+	private int age;
+	private int weight;
+	private String animalType;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getAnimalType() {
+		return animalType;
+	}
+	public void setAnimalType(String animalType) {
+		this.animalType = animalType;
+	}
+	@Override
+	public String toString() {
+		return "Animal [age=" + age + ", weight=" + weight + ", animalType=" + animalType + "]";
+	}
+	
+	
 }
--- Day3Austin/src/com/revature/code/Driver.java
+++ Day3Austin/src/com/revature/code/Driver.java
@@ -4,7 +4,13 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	Animal a = new Animal();
+	System.out.println(a);
+	a.setAge(80);
+	a.setAnimalType("Dog");
+	a.setWeight(180);
+	System.out.println(a.getAnimalType());
+	
 	}
 
 }
